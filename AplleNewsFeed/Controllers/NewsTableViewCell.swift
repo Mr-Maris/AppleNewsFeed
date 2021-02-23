09fd9aa0a3b9ac320ec5111f7d93569e89090c92
Mr-Maris <atver404@gmail.com>
2021-02-23 14:41:06 +0200

    first commit

---
+++ AplleNewsFeed/Controllers/NewsTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  NewsTableViewCell.swift
+//  AplleNewsFeed
+//
+//  Created by Maris Rasnacs on 23/02/2021.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var newsImageView: UIImageView!
+    
+    @IBOutlet weak var newsTitleLabel: UILabel!
+    
+}
